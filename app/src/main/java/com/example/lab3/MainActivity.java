5b825c6647ae8680888ba7ad0d942565f7caa71e
FrancesCC <holyspirit1515@gmail.com>
2020-10-01 22:02:08 -0400

    adding second activity, display email on second activity page.

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,13 +2,69 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences prefs = null;  //obj
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        String savedString = prefs.getString("emailAdd", "Default Value");
+        EditText typeField = findViewById(R.id.emailID);
+        typeField.setText(savedString);
+
+        Button saveButton = findViewById(R.id.logButton);
+
+        saveButton.setOnClickListener(bt ->
+                {
+                    // creates a transition to load profileActivity.java
+                    Intent goToProfile  = new Intent(MainActivity.this,ProfileActivity.class);
+                    goToProfile.putExtra("EMAIL", savedString);
+                    saveSharedPrefs(typeField.getText().toString());
+
+                    startActivity(goToProfile);    //go to SecondActivity.java
+                });
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+//        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+//        String savedString = prefs.getString("emailAdd", "Default Value");
+//        EditText typeField = findViewById(R.id.emailID);
+//        typeField.setText(savedString);
+//
+//        Button saveButton = findViewById(R.id.logButton);
+//        saveButton.setOnClickListener(bt ->
+//                {
+//                    // creates a transition to load profileActivity.java
+//                    Intent goToProfile  = new Intent(MainActivity.this,ProfileActivity.class);
+//                    //goToProfile.putExtra("EMAIL",typeField.getText().toString());
+//                    saveSharedPrefs(typeField.getText().toString());
+//
+//                    startActivity(goToProfile);    //go to SecondActivity.java
+//                });
+    }
+
+
+    private void saveSharedPrefs(String stringToSave) {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("emailAdd", stringToSave);
+        editor.commit();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/lab3/ProfileActivity.java
@@ -0,0 +1,93 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //This gets you the object nextPage from FirstActivity.java
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("EMAIL");
+        EditText emailText = findViewById(R.id.emailInput);
+        emailText.setText(email);
+
+        ImageButton imgBtn = findViewById(R.id.imgButton);
+        imgBtn.setOnClickListener( click -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+            finish();
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e(ACTIVITY_NAME, "In function: onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e(ACTIVITY_NAME, "In function: onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(ACTIVITY_NAME, "In function: onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(ACTIVITY_NAME, "In function: onDestroy");
+        super.onDestroy();
+    }
+
+    //sending data back to previous page
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
+}
